5e271e1f53744e117428b830b7a0304e8bae1ceb
phenix1229 <paul.garay@codeimmersives.com>
2020-03-23 18:55:32 -0400

    testing heroku

--- app.js
+++ app.js
@@ -16,13 +16,23 @@ require('./lib/passport');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser:true,
-  useUnifiedTopology:true,
-  useCreateIndex:true
-}).then(()=>{
-  console.log('MongoDB connected');
-}).catch(err => console.log(`Mongo Error: ${err}`));
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser:true,
+//   useUnifiedTopology:true,
+//   useCreateIndex:true
+// }).then(()=>{
+//   console.log('MongoDB connected');
+// }).catch(err => console.log(`Mongo Error: ${err}`));
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://pgaray1229:Newyer98!@cluster0-dnsdm.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
