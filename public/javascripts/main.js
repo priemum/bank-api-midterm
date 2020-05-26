91e721c398befb85658a3790269789451d465b7b
phenix1229 <paul.garay@codeimmersives.com>
2020-05-26 19:20:16 -0400

    changes to menu, added menu and balances css files, removed duplicate code from remaining css files, updated ejs files to use new css files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,20 @@
-document.querySelector('#menu').addEventListener('click', function(){
-    const optionsDiv = document.querySelector('#menuOptions');
-    if(optionsDiv.style.display === 'none'){
-        optionsDiv.style.display = 'block';
-    } else {
-        optionsDiv.style.display = 'none';
-    }
+const menuItemsText = document.querySelectorAll('.mOptionText')
+
+document.querySelector('#menuText').addEventListener('mouseover', function(){
+    document.querySelector('#menuOptions').style.display = 'block';
 })
+
+document.querySelector('#menuOptions').addEventListener('mouseleave', function(){
+    document.querySelector('#menuOptions').style.display = 'none';
+})
+
+
+menuItemsText.forEach(item => item.addEventListener('mouseover', function(){
+    item.style.backgroundColor = '#43505D';
+    item.style.color = '#FFFFFF';
+}))
+
+menuItemsText.forEach(item => item.addEventListener('mouseleave', function(){
+    item.style.backgroundColor = 'rgb(166, 204, 255)';
+    item.style.color = '#43505D';
+}))
